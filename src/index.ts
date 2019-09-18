ba0b6e96d4261c188c4304e24e4b9207c2f43dcf
ermondel <ermondel@gmail.com>
2019-09-18 11:18:48 +0300

    feat: analyzer implementation

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,17 @@
-import { MatchReader } from "./MatchReader";
-import { MatchResult } from "./MatchResult";
-import { CsvFileReader } from "./CsvFileReader";
+import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CsvFileReader';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { Summary } from './Summary';
 
-const csvFileReader = new CsvFileReader("football.csv");
+const csvFileReader = new CsvFileReader('football.csv');
 
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-let manUnitedWins = 0;
+const summary = new Summary(
+  new WinsAnalysis('Man United'),
+  new ConsoleReport()
+);
 
-for (let match of matchReader.matches) {
-  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`| length ${matchReader.matches.length} |`);
-const msg = `Man United won ${manUnitedWins} games`;
-console.log(msg);
+summary.buildAndPrintReport(matchReader.matches);
